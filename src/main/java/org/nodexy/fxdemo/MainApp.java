d8711ab73e40a9e8046e2891323d9a97191381a3
Monde Ngalonkulu <monde.ngalonkulu@nimbletech.co.za>
2016-10-27 18:21:20 +0200

    Initial commit. A simple JavaFX program

---
+++ src/main/java/org/nodexy/fxdemo/MainApp.java
@@ -0,0 +1,29 @@
+package org.nodexy.fxdemo;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ * Created by phoenix on 10/27/16.
+ */
+public class MainApp extends Application {
+    /**
+     * The main method is only needed for the IDE with limited
+     * JavaFX support. Not needed for running from the command line.
+     */
+    /*public static void main(String[] args) {
+        Application.launch(args);
+    }*/
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Create a scene and place a button onto the scene.
+        Button btnOk = new Button("OK");
+        Scene scene = new Scene(btnOk, 200, 250);
+        primaryStage.setTitle("JavaFX Demo");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
